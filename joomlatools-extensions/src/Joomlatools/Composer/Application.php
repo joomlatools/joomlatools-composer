251add619fa59bfee1093cee4e28478eecbce2a6
Steven Rombauts <steven@timble.net>
2013-08-09 00:54:13 +0200

    Initial commit

---
+++ joomlatools-extensions/src/Joomlatools/Composer/Application.php
@@ -0,0 +1,110 @@
+<?php
+namespace Joomlatools\Composer;
+
+use \JApplicationCli as JApplicationCli;
+use \JFactory as JFactory;
+use \JDispatcher as JDispatcher;
+use \JPluginHelper as JPluginHelper;
+use \JAuthentication as JAuthentication;
+
+class Application extends JApplicationCli
+{
+    protected $_messageQueue = array();
+
+    public function initialise($options = array())
+    {
+        jimport('joomla.application.component.helper');
+        jimport('joomla.application.menu');
+        jimport('joomla.environment.uri');
+        jimport('joomla.event.dispatcher');
+        jimport('joomla.utilities.utility');
+        jimport('joomla.utilities.arrayhelper');
+
+        //JPluginHelper::importPlugin('system');
+    }
+
+    public function login($credentials, $options = array())
+    {
+        jimport('joomla.user.authentication');
+
+        $authenticate = JAuthentication::getInstance();
+        $response	  = $authenticate->authenticate($credentials, $options);
+
+        if($response->status === JAUTHENTICATE_STATUS_SUCCESS)
+        {
+            $session = JFactory::getSession();
+
+            // we fork the session to prevent session fixation issues
+            $session->fork();
+
+            $this->_createSession($session->getId());
+
+            // Import the user plugin group
+            JPluginHelper::importPlugin('user');
+
+            $results = $this->triggerEvent('onLoginUser', array((array)$response, $options));
+
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function _createSession($name)
+    {
+        $options = array();
+        $options['name'] = $name;
+
+        $session = JFactory::getSession($options);
+
+        jimport('joomla.database.table');
+        $storage = JTable::getInstance('session');
+        $storage->purge($session->getExpire());
+
+        // Session exists and is not expired, update time in session table
+        if ($storage->load($session->getId())) {
+            $storage->update();
+            return $session;
+        }
+
+        //Session doesn't exist yet, initalise and store it in the session table
+        $session->set('registry',	new \JRegistry('session'));
+        $session->set('user',		new \JUser());
+
+        if(!$storage->insert( $session->getId(), 1)) {
+            jexit( $storage->getError());
+        }
+
+        return $session;
+    }
+
+    public function getCfg($varname, $default = null)
+    {
+        return \JFactory::getConfig()->get('' . $varname, $default);
+    }
+
+    public function enqueueMessage($msg, $type = 'message')
+    {
+        $this->_messageQueue[] = array('message' => $msg, 'type' => strtolower($type));
+    }
+
+    public function getMessageQueue()
+    {
+        return $this->_messageQueue;
+    }
+
+    public function getName()
+    {
+        return 'cli';
+    }
+
+    public function isSite()
+    {
+        return false;
+    }
+
+    public function isAdmin()
+    {
+        return true;
+    }
+}
---
+++ joomlatools-extensions/src/Joomlatools/Composer/ExtensionInstaller.php
@@ -0,0 +1,119 @@
+<?php
+namespace Joomlatools\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+use Composer\Installer\LibraryInstaller;
+
+class ExtensionInstaller extends LibraryInstaller
+{
+    protected $_credentials = array();
+
+    public function __construct(IOInterface $io, Composer $composer, $type = 'library')
+    {
+        parent::__construct($io, $composer, $type);
+
+        $username = $composer->getConfig()->get('joomla-username');
+        $password = $composer->getConfig()->get('joomla-password');
+
+        if(!empty($username) && !empty($password))
+        {
+            $this->_credentials = array(
+                'username' => $composer->getConfig()->get('joomla-username'),
+                'password' => $composer->getConfig()->get('joomla-password'),
+            );
+        }
+    }
+
+    public function getInstallPath(PackageInterface $package)
+    {
+        return 'tmp/' . $package->getPrettyName();
+    }
+
+    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        // Install the package into the temporary directory, so we can access all it's files
+        parent::install($repo, $package);
+
+        // Now install into the Joomla environment
+        $this->_bootstrap();
+
+        $installer = new \JInstaller();
+
+        if(!$installer->install($this->getInstallPath($package)))
+        {
+            $descriptions = $this->_getApplicationMessages();
+
+            $error = 'Error while installing '.$package->getPrettyName();
+            if(count($descriptions)) {
+                $error .= ':'.PHP_EOL.implode(PHP_EOL, $descriptions);
+            }
+
+            throw new \UnexpectedValueException($error);
+        }
+    }
+
+    public function supports($packageType)
+    {
+        return 'joomlatools-extension' === $packageType;
+    }
+
+    protected function _bootstrap()
+    {
+        if(defined('_JEXEC')) {
+            return;
+        }
+
+        $_SERVER['HTTP_HOST']   = 'localhost';
+
+        define('_JEXEC', 1);
+        define('DS', DIRECTORY_SEPARATOR);
+
+        define('JPATH_BASE', realpath('.'));
+        require_once JPATH_BASE . '/includes/defines.php';
+
+        require_once JPATH_BASE . '/includes/framework.php';
+        require_once JPATH_LIBRARIES . '/import.php';
+
+        require_once JPATH_LIBRARIES . '/cms.php';
+
+        error_reporting(0);
+
+        $application = \JFactory::getApplication('administrator');
+        $application->initialise();
+
+        $this->_authenticate();
+    }
+
+    protected function _authenticate()
+    {
+        $application = \JFactory::getApplication();
+
+        if(empty($this->_credentials['username']) || empty($this->_credentials['password'])) {
+            return false;
+        }
+
+        if($application->login($this->_credentials) !== true) {
+            throw new \KException('Login failed for user ' . $this->_credentials['username'], \KHttpResponse::UNAUTHORIZED);
+        }
+
+        return true;
+    }
+
+    protected function _getApplicationMessages()
+    {
+        $messages = \JFactory::getApplication()->getMessageQueue();
+        $descriptions = array();
+
+        foreach($messages as $message)
+        {
+            if($message['type'] == 'error') {
+                $descriptions[] = $message['message'];
+            }
+        }
+
+        return $descriptions;
+    }
+}
