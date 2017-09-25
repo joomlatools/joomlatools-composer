f94d1c0caee09d8ab64b644f81a494027fa0b08d
Steven Rombauts <steven@timble.net>
2017-09-25 16:38:24 +0100

    #35: Better logging

--- _travis/script.sh
+++ _travis/script.sh
@@ -6,15 +6,22 @@ echo "** Installing test extension"
 composer require -vv --working-dir=$DOCUMENTROOT/testsite --no-interaction joomlatools/composer-helloworld:dev-testbranch
 
 # Verify if component file is present
-echo "** Looking for helloworld.php"
 FILE="$DOCUMENTROOT/testsite/administrator/components/com_helloworld/helloworld.php"
+echo "** Looking for $FILE"
 if [ "$REPO" == "platform" ]; then
   FILE="$DOCUMENTROOT/testsite/app/administrator/components/com_helloworld/helloworld.php"
 fi
 
 echo "** Verifying helloworld.php contents"
-[ -f $FILE ] && true || false
-grep -q "echo 'Hello World\!'" $FILE && true || false
+if [ ! -f $FILE ]; then
+    echo "$FILE does not exist"
+    exit 1
+fi
+
+if ! grep -q "echo 'Hello World\!'" $FILE; then
+    echo "$FILE does not contain string"
+    exit 1
+fi
 
 # Test if the row exists in the database
 echo "** Verifying extensions row in database"
@@ -25,4 +32,7 @@ fi
 
 COUNT=$(mysql -uroot -s -N -e "SELECT COUNT(extension_id) FROM sites_testsite.j_extensions WHERE element = 'com_helloworld';")
 echo "Matched $COUNT rows"
-[ $COUNT -gt 0 ] && true || false
+
+if [ $COUNT -le 0 ]; then
+   exit 1
+fi
