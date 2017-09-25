f836b0defa43d4ac04a160ec46c77d1c5967a0e3
Steven Rombauts <steven@timble.net>
2017-09-25 13:51:17 +0100

    #35: Fix path to helloworld.php

--- _travis/script.sh
+++ _travis/script.sh
@@ -7,7 +7,7 @@ composer require -v --working-dir=$DOCUMENTROOT/testsite --no-interaction joomla
 
 # Verify if component file is present
 [ -f $DOCUMENTROOT/testsite/administrator/components/com_helloworld/helloworld.php ] && true || false
-grep -q "echo 'Hello World\!'" helloworld.php && true || false
+grep -q "echo 'Hello World\!'" $DOCUMENTROOT/testsite/administrator/components/com_helloworld/helloworld.php && true || false
 
 # Test if the row exists in the database
 COUNT=$(mysql -uroot -s -N -e "SELECT COUNT(extension_id) FROM sites_testsite.j_extensions WHERE element = 'com_helloworld';")
