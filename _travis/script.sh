d56985379db67ce899ba6e5479caebed8cd74d03
Steven Rombauts <steven@timble.net>
2017-09-25 14:56:37 +0100

    #35: Remove uninstall tests
    Uninstall is broken at the moment and requires more
    work in ticket #36

--- _travis/script.sh
+++ _travis/script.sh
@@ -13,16 +13,3 @@ grep -q "echo 'Hello World\!'" $DOCUMENTROOT/testsite/administrator/components/c
 COUNT=$(mysql -uroot -s -N -e "SELECT COUNT(extension_id) FROM sites_testsite.j_extensions WHERE element = 'com_helloworld';")
 echo "Matched $COUNT rows\n"
 [ $COUNT -gt 0 ] && true || false
-
-echo "** Uninstalling test extension"
-composer remove -v --no-update --working-dir=$DOCUMENTROOT/testsite --no-interaction joomlatools/composer-helloworld
-
-# Verify if component file has been removed
-[ ! -f $DOCUMENTROOT/testsite/administrator/components/com_helloworld/helloworld.php ] && true || false
-
-# Ensure extensions table has been updated
-COUNT=$(mysql -uroot -s -N -e "SELECT COUNT(extension_id) FROM sites_testsite.j_extensions WHERE element = 'com_helloworld';")
-echo "Matched $COUNT rows\n"
-
-[ $COUNT -eq 0 ] && true || false
-
