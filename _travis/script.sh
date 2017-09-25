a993ada06f3ac50882fd061417a20e3bfb82f904
Steven Rombauts <steven@timble.net>
2017-09-25 16:55:33 +0100

    #35: Fix database name for platform

--- _travis/script.sh
+++ _travis/script.sh
@@ -2,15 +2,19 @@
 
 set -e
 
+FILE="$DOCUMENTROOT/testsite/administrator/components/com_helloworld/helloworld.php"
+DB="sites_testsite.j_extensions"
+
+if [[ "$REPO" == *"joomlatools/joomlatools-platform"* ]]; then
+  FILE="$DOCUMENTROOT/testsite/app/administrator/components/com_helloworld/helloworld.php"
+  DB="sites_testsite.extensions"
+fi
+
 echo "** Installing test extension"
 composer require -vv --working-dir=$DOCUMENTROOT/testsite --no-interaction joomlatools/composer-helloworld:dev-testbranch
 
 # Verify if component file is present
-FILE="$DOCUMENTROOT/testsite/administrator/components/com_helloworld/helloworld.php"
-echo "** Looking for $FILE"
-if [[ "$REPO" == *"joomlatools/joomlatools-platform"* ]]; then
-  FILE="$DOCUMENTROOT/testsite/app/administrator/components/com_helloworld/helloworld.php"
-fi
+echo "** Looking for helloworld.php"
 
 echo "** Verifying helloworld.php contents"
 if [ ! -f $FILE ]; then
@@ -25,12 +29,8 @@ fi
 
 # Test if the row exists in the database
 echo "** Verifying extensions row in database"
-DB="sites_testsite.j_extensions"
-if [ "$REPO" == "platform" ]; then
-  DB="sites_testsite.extensions"
-fi
 
-COUNT=$(mysql -uroot -s -N -e "SELECT COUNT(extension_id) FROM sites_testsite.j_extensions WHERE element = 'com_helloworld';")
+COUNT=$(mysql -uroot -s -N -e "SELECT COUNT(extension_id) FROM sites_testsite.$DB WHERE element = 'com_helloworld';")
 echo "Matched $COUNT rows"
 
 if [ $COUNT -le 0 ]; then
