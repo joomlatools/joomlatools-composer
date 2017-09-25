997d114fcb158862e388a3814ebe4d5d7b36cf34
Steven Rombauts <steven@timble.net>
2017-09-25 15:41:16 +0100

    #35: Add more logging statemetns

--- _travis/script.sh
+++ _travis/script.sh
@@ -3,18 +3,21 @@
 set -e
 
 echo "** Installing test extension"
-composer require -v --working-dir=$DOCUMENTROOT/testsite --no-interaction joomlatools/composer-helloworld:dev-testbranch
+composer require -vv --working-dir=$DOCUMENTROOT/testsite --no-interaction joomlatools/composer-helloworld:dev-testbranch
 
 # Verify if component file is present
+echo "** Looking for helloworld.php"
 FILE="$DOCUMENTROOT/testsite/administrator/components/com_helloworld/helloworld.php"
 if [ "$REPO" == "platform" ]; then
   FILE="$DOCUMENTROOT/testsite/app/administrator/components/com_helloworld/helloworld.php"
 fi
 
+echo "** Verifying helloworld.php contents"
 [ -f $DOCUMENTROOT/testsite/administrator/components/com_helloworld/helloworld.php ] && true || false
 grep -q "echo 'Hello World\!'" $DOCUMENTROOT/testsite/administrator/components/com_helloworld/helloworld.php && true || false
 
 # Test if the row exists in the database
+echo "** Verifying extensions row in database"
 DB="sites_testsite.j_extensions"
 if [ "$REPO" == "platform" ]; then
   DB="sites_testsite.extensions"
