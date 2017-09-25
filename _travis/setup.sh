4f84f6de26f01dde3eb19923d843efb4197a0185
Steven Rombauts <steven@timble.net>
2017-09-25 12:37:35 +0100

    #35: Move to separate script

--- _travis/setup.sh
+++ _travis/setup.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
 set -e 
-  
-echo "Configuring PHP"
-phpenv config-rm xdebug.ini
-echo 'error_reporting = 22519' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 
-echo "Create documentroot directory"
-mkdir -p $DOCUMENTROOT
+joomla site:create --www=$DOCUMENTROOT --mysql-login="root" composer
+composer require -v --working-dir=$DOCUMENTROOT/composer --no-interaction joomlatools/composer-helloworld
+
+# Verify if component file is present
+[ -f $DOCUMENTROOT/composer/administrator/components/com_helloworld/helloworld.php ] && true || false
+grep -q "echo 'Hello World\!'" helloworld.php && true || false
+
+# Test if the row exists in the database
+COUNT=$(mysql -uroot -s -N -e "SELECT COUNT(extension_id) FROM sites_composer.j_extensions WHERE element = 'com_helloworld';")
+echo "Matched $COUNT rows\n"
+"[ $COUNT -gt 0 ] && true || false"
+
+# Test uninstall
+composer remove -v --working-dir=$DOCUMENTROOT/composer --no-interaction joomlatools/composer-helloworld
+
+# Verify if component file has been removed
+[ ! -f $DOCUMENTROOT/composer/administrator/components/com_helloworld/helloworld.php ] && true || false
+
+# Ensure extensions table has been updated
+COUNT=$(mysql -uroot -s -N -e "SELECT COUNT(extension_id) FROM sites_composer.j_extensions WHERE element = 'com_helloworld';")
+echo "Matched $COUNT rows\n"
+[ $COUNT -eq 0 ] && true || false
+
