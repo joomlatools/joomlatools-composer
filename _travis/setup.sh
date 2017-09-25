ee573dcef21f130ce2facd5959ad7cb4c8d1a8a7
Steven Rombauts <steven@timble.net>
2017-09-25 13:27:59 +0100

    #35: Make sure to keep package name and type

--- _travis/setup.sh
+++ _travis/setup.sh
@@ -20,23 +20,21 @@ cd /tmp/joomlatools-composer-helloworld/
 git checkout -b testbranch
 cat >/tmp/joomlatools-composer-helloworld/composer.json <<EOL
 {
+    "name": "joomlatools/composer-helloworld",
+    "type": "joomlatools-composer",
     "require": {
          "joomlatools/composer": "dev-testbranch"
     }
 }
 EOL
 
-cat /tmp/joomlatools-composer-helloworld/composer.json
-
 git commit -a -m "Add test dependencies"
 
 echo "** Set up test site"
 joomla site:create --www=$DOCUMENTROOT --mysql-login="root" testsite
-composer --working-dir=$DOCUMENTROOT/testsite config repositories.plugin vcs file:///$TRAVIS_BUILD_DIR
-composer --working-dir=$DOCUMENTROOT/testsite config repositories.component vcs file:////tmp/joomlatools-composer-helloworld/
-composer --working-dir=$DOCUMENTROOT/testsite config minimum-stability dev
-
-cat $DOCUMENTROOT/testsite/composer.json
+composer --no-interaction --working-dir=$DOCUMENTROOT/testsite config repositories.plugin vcs file:///$TRAVIS_BUILD_DIR
+composer --no-interaction --working-dir=$DOCUMENTROOT/testsite config repositories.component vcs file:////tmp/joomlatools-composer-helloworld/
+composer --no-interaction --working-dir=$DOCUMENTROOT/testsite config minimum-stability dev
 
 # Reset working directory
 cd $TRAVIS_BUILD_DIR
