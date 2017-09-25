f9728467f6dc3878b6a0d1fd0f9d494bc1ab3321
Steven Rombauts <steven@timble.net>
2017-09-25 12:54:08 +0100

    #35: Test the actual branch instead of from latest release

--- _travis/script.sh
+++ _travis/script.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-# Install
-joomla site:create --www=$DOCUMENTROOT --mysql-login="root" composer
-composer require -v --working-dir=$DOCUMENTROOT/composer --no-interaction joomlatools/composer-helloworld
+echo "Installing test extension"
+composer require -v --working-dir=$DOCUMENTROOT/testsite --no-interaction joomlatools/composer-helloworld:dev-testbranch
 
 # Verify if component file is present
 [ -f $DOCUMENTROOT/composer/administrator/components/com_helloworld/helloworld.php ] && true || false
 grep -q "echo 'Hello World\!'" helloworld.php && true || false
 
 # Test if the row exists in the database
-COUNT=$(mysql -uroot -s -N -e "SELECT COUNT(extension_id) FROM sites_composer.j_extensions WHERE element = 'com_helloworld';")
+COUNT=$(mysql -uroot -s -N -e "SELECT COUNT(extension_id) FROM sites_testsite.j_extensions WHERE element = 'com_helloworld';")
 echo "Matched $COUNT rows\n"
 "[ $COUNT -gt 0 ] && true || false"
 
@@ -22,7 +21,7 @@ composer remove -v --working-dir=$DOCUMENTROOT/composer --no-interaction joomlat
 [ ! -f $DOCUMENTROOT/composer/administrator/components/com_helloworld/helloworld.php ] && true || false
 
 # Ensure extensions table has been updated
-COUNT=$(mysql -uroot -s -N -e "SELECT COUNT(extension_id) FROM sites_composer.j_extensions WHERE element = 'com_helloworld';")
+COUNT=$(mysql -uroot -s -N -e "SELECT COUNT(extension_id) FROM sites_testsite.j_extensions WHERE element = 'com_helloworld';")
 echo "Matched $COUNT rows\n"
 [ $COUNT -eq 0 ] && true || false
 
--- _travis/setup.sh
+++ _travis/setup.sh
@@ -8,3 +8,30 @@ echo 'error_reporting = 22519' >> ~/.phpenv/versions/$(phpenv version-name)/etc/
 
 echo "Create documentroot directory"
 mkdir -p $DOCUMENTROOT
+
+echo "Checkout test branch"
+git checkout -b testbranch
+
+# Based on the instructions from https://github.com/joomlatools/joomlatools-composer/wiki
+echo "Set up joomlatools/composer-helloworld"
+git clone git@github.com:joomlatools/joomlatools-composer-helloworld.git /tmp/
+
+cd /tmp/joomlatools-composer-helloworld/
+git checkout -b testbranch
+cat >composer.json <<EOL
+{
+    "require": {
+         "joomlatools/composer": "dev-testbranch"
+    }
+}
+EOL
+git commit -a -m "Add test dependencies"
+
+echo "Set up test site"
+joomla site:create --www=$DOCUMENTROOT --mysql-login="root" composer
+composer --working-dir=$DOCUMENTROOT/composer config repositories.plugin vcs file:///$TRAVIS_BUILD_DIR
+composer --working-dir=$DOCUMENTROOT/composer config repositories.component vcs /tmp/joomlatools-composer-helloworld/
+composer --working-dir=$DOCUMENTROOT/composer config minimum-stability dev
+
+# Reset working directory
+cd $TRAVIS_BUILD_DIR
