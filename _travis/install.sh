81dd22f531c7b19f736b7228c3f102f7dad4f8ac
Steven Rombauts <steven@timble.net>
2018-12-30 17:36:34 +0000

    Debug Travis

--- _travis/install.sh
+++ _travis/install.sh
@@ -2,6 +2,11 @@
 
 set -e 
 
+echo $PATH
+
 echo "** Installing joomlatools/console"
 composer global require --no-interaction joomlatools/console
+
+ls -lah /home/travis/.composer/vendor/bin/
+
 joomla -V
