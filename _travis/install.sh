21b32161a76e74d6b6e07951733d194d83140fe7
Steven Rombauts <steven@timble.net>
2018-12-30 17:45:05 +0000

    Call 'joomla' command using 'composer exec'
    Due to a recent change in the Travis build environment, the
    path to Composer's global bin directory has changed.
    
    The suggested fix is to use 'composer exec' instead of
    relying on the hardcoded path.
    
    See: https://github.com/travis-ci/travis-ci/issues/7289

--- _travis/install.sh
+++ _travis/install.sh
@@ -2,11 +2,7 @@
 
 set -e 
 
-echo $PATH
-
 echo "** Installing joomlatools/console"
 composer global require --no-interaction joomlatools/console
 
-ls -lah $HOME/.composer/vendor/bin/
-
-joomla -V
+composer global exec joomla -V
